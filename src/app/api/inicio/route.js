4c80e90f55f10976f8cc145b0ee05685ae314da4
Hugo Oyola <hugo.ham@outlook.com>
2024-02-11 01:51:29 -0500

    Corregir importaciones en archivos de ruta de API

--- src/app/api/inicio/route.js
+++ src/app/api/inicio/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs } from "firebase/firestore";
-import {db} from "@/firebase/config";
+import { db } from "@/firebase/config";
 
-export async function GET(_,) {
+export async function GET(_) {
   const productsRef = collection(db, "promociones");
   const querySnapshot = await getDocs(productsRef);
   const promos = querySnapshot.docs.map((doc) => doc.data());
--- src/app/api/productos/route.js
+++ src/app/api/productos/route.js
@@ -1,29 +1,11 @@
-// pages/api/products.js
 import { NextResponse } from "next/server";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebase/config";
 
-export default async function GET(req, res) {
-    const productsRef = collection(db, "productos");
-    const querySnapshot = await getDocs(productsRef);
-    const docs = querySnapshot.docs.map((doc) => doc.data());
+export async function GET(_) {
+  const productsRef = collection(db, "productos");
+  const querySnapshot = await getDocs(productsRef);
+  const products = querySnapshot.docs.map((doc) => doc.data());
 
-    return NextResponse.json(docs);
+  return NextResponse.json(products);
 }
-
-
-// import { NextResponse } from "next/server";
-// import { collection, getDocs, query, where } from "firebase/firestore";
-// import {db} from "@/firebase/config";
-
-// export async function GET(_, { params }) {
-//   const { category } = params;
-//   const productsRef = collection(db, "productos");
-//   const q = category === "todos" ? productsRef : query(productsRef, where("category", "==", category));
-
-//   const querySnapshot = await getDocs(q);
-
-//   const docs = querySnapshot.docs.map((doc) => doc.data());
-
-//   return NextResponse.json(docs);
-// }
