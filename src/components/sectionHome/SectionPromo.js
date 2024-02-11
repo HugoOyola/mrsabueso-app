4a2df55b9282909f816a37fb65e4a1eead43da3b
Hugo Oyola <hugo.ham@outlook.com>
2024-02-11 16:22:40 -0500

    ✨ Eliminar ruta de productos y corregir descripción de promociones

--- src/components/sectionHome/SectionPromo.js
+++ src/components/sectionHome/SectionPromo.js
@@ -1,20 +1,20 @@
 import Image from "next/image";
 
 const OfertasSection = async ({promociones}) => {
-  const response = await fetch(`http://localhost:3000/api/inicio`, { cache: "force-cache" }).then((r) => r.json());
+  const response = await fetch(`http://localhost:3000/api/inicio`, { cache: "force-cache" }).then( r => r.json());
   console.log(response);
 
   return (
     <section className="bg-white p-4 wrapper">
       <h2 className="text-xl font-bold text-azul">¡Lleva más, paga menos con estas ofertas!</h2>
       <div className="mt-4 grid grid-cols-3 gap-4">
-        {promociones.map((promocion, index) => (
+        {response.map((promocion, index) => (
           <div key={index} className="w-full">
             <div>
               <Image
                 alt={`Oferta ${index + 1}`}
                 className="w-full h-auto hover:shadow-lg"
-                src={promocion.imagen}
+                src={`/${promocion.imagen}`}
                 width={500}
                 height={300}
                 sizes="(min-width: 768px) 33vw, 100vw"
--- src/data/promociones.js
+++ src/data/promociones.js
@@ -1,5 +1,5 @@
 export const promociones = [
-  { imagen: '/banner-oferta-snacks.jpg', descripcion: 'Ofertas en alimentos seleccionados' },
+  { imagen: '/banner-oferta-snacks.jpg', descripcion: ' en alimentos seleccionados' },
   { imagen: '/banner-juguetes.jpg', descripcion: 'Descuentos únicos en juguetes seleccionados' },
   { imagen: '/banner-alimento-perro-gato.jpg', descripcion: 'Combos especiales para tu mascota' },
   // Agrega más ofertas según sea necesario
