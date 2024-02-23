2dd26b75477e27565c1669460ee68bf73941bda9
Hugo Oyola <hugo.ham@outlook.com>
2024-02-23 12:12:10 -0500

    Agrega imágenes y mejora la visualización de los productos por tipo de mascota

--- src/components/uiPetType/PetProducts.js
+++ src/components/uiPetType/PetProducts.js
@@ -1,5 +1,9 @@
 import ProductCard from "../uiProducts/ProductCard";
 
+const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 const PetProducts = async ({petType}) => {
   const products = await fetch(`http://localhost:3000/api/mascota/${petType}`, { cache: "force-cache", next:{
     tags:['productos']
@@ -9,7 +13,7 @@ const PetProducts = async ({petType}) => {
 
   return (
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 className="text-2xl font-semibold mb-4">Productos para Perros</h2>
+      <h2 className="text-2xl font-semibold mb-4">Todo los productos para tu {capitalize(petType)} 🐾</h2>
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
           <li key={product.id}>
