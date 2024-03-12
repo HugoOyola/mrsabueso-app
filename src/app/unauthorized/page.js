6ac3e13a6f7c2fc692e1d744c91a867ed27750a1
Hugo Oyola <hugo.ham@outlook.com>
2024-03-12 11:33:02 -0500

    Actualizar página de acceso no autorizado

--- src/app/unauthorized/page.js
+++ src/app/unauthorized/page.js
@@ -1,5 +1,18 @@
 const Unauthorized = () => {
-  return <div>Usuario no autorizado</div>;
+  return (
+    <div>
+      <div className="h-[70px]"></div>
+      <div class="bg-gray-200 h-screen flex justify-center items-center">
+        <div class="bg-white p-8 rounded-lg shadow-md">
+          <h1 class="text-2xl font-semibold mb-4">¡Acceso no autorizado!</h1>
+          <p class="text-gray-600 mb-4">Debes iniciar sesión para acceder a esta página.</p>
+          <a href="/" class="text-blue-500 hover:underline">
+            Volver al inicio
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Unauthorized;
